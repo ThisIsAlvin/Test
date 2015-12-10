561c6f0feb125278ce22a08db2f48ca3543e371b
alvin-git <2379243059@qq.com>
2015-12-10 16:12:29 +0800

    一个空的web工程

---
+++ src/com/LoginAction.java
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: LoginAction.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2015年12月01日 星期二 21时13分04秒
+ ************************************************************************/
+package com;
+import com.*;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport
+{
+   private User user; 
+
+   public User getUser()
+   {
+       retrun user;
+   }
+   public void setUser(User user)
+   {
+       this.user = user;
+   }
+
+
+}
+
---
+++ src/com/User.java
@@ -0,0 +1,61 @@
+/*************************************************************************
+	> File Name: User.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2015年12月01日 星期二 21时13分50秒
+ ************************************************************************/
+package com;
+
+public class User
+{
+    private String name;
+    private String passWord;
+    private int tel;
+    private String sex;
+    private String email;
+
+   public String getName()
+   {
+    return name;
+   }
+   public void setName(String name)
+   {
+
+       this.name = name;
+   }
+   public String getPassWord()
+   {
+       return passWord;
+   }
+   public void setPassWord(String passWord)
+    {
+        this.passWord = passWord;
+    }
+   public int getTel()
+   {
+       return tel;
+   }
+   public void setTel(int tel)
+   {
+       this.tel = tel;
+   }
+   public String getSex()
+   {
+       return sex;
+   }
+   public void setSex(String sex)
+   {
+       this.sex = sex;
+   }
+   public String getEmail()
+   {
+       return email;
+   }
+   public void setEmail(String email)
+   {
+       this.email = email;
+   }
+}
+
+
+
